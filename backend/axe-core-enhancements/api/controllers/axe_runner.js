0bf4ebaee7a1ebe91a8b64dc8d273f6d899fb7a5
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-05-24 18:58:52 -0700

    Adding async functionality, testing the parsing of multiple links

--- backend/axe-core-enhancements/api/controllers/axe_runner.js
+++ backend/axe-core-enhancements/api/controllers/axe_runner.js
@@ -7,16 +7,22 @@ class axe_runner{
   run(url_list) {
     (async () => {
       //note: make sure to run node_modules/puppeteer/install.js to get both firefox and chrome switching available
-      const browser = await puppeteer.launch({product: 'firefox',headless:false});
-      const page = await browser.newPage();
-      await page.goto(url_list[0]);
-      const results = await new AxePuppeteer(page).analyze();
-      console.log(results.violations);
-      await page.close();
+      const browser = await puppeteer.launch({product: 'firefox',headless:true});
+      const violations = (await Promise.allSettled(
+        [...Array(url_list.length)].map(async(_,i) => {
+          const page = await browser.newPage();
+          await page.goto(url_list[i]);
+          const results = await new AxePuppeteer(page).analyze();
+          return results.violations[0].id;
+        })
+      )).filter(e =>e.status === "fulfilled")
+        .map(e => e.value);
+      console.log(violations);
       await browser.close();
     })();
   }
 }
+
 
 const runner = new axe_runner();
 
