32bfeb0fb73574b3b1251ee2bacbcf28fd8fca36
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-05-26 21:59:57 -0700

    Added description + comments

--- backend/axe-core-enhancements/api/controllers/axe-runner.js
+++ backend/axe-core-enhancements/api/controllers/axe-runner.js
@@ -1,17 +1,29 @@
+/*
+* Axe Runner
+* Author: Michael Kennedy
+* Description: Driver function for the Axe-Core accessibility testing engine.
+* Parameters: name, tags, url_list.
+* Name: name of the browser to be used (either firefox, or chome)
+* Tags: wcag success criteria
+* Url_list: list of 1...* URLs to parse
+*
+* This application utilizes axe-core/puppeteer to run axe-core on the listed URLs.
+* Upon testing, the results from headless and non-headless puppeteer were the same, whereas headless selenium vs non-headless
+* yielded slightly different results.
+*
+* At this point in time, this only can use Firefox and Chrome. It's possible for puppeteer to use Microsoft Edge
+* however it will need to be installed post-deployment, most likely in a later release.
+* */
+
+
 const  {AxePuppeteer} = require ('@axe-core/puppeteer');
 const puppeteer = require ('puppeteer');
 const AxeBuilder = require('@axe-core/webdriverjs');
 const WebDriver = require('selenium-webdriver');
 const chrome_driver = require('chromedriver');
-const edge_driver = require('edgedriver');
 const  {AceResult} = require('../models/axe-result.js');
 
-/*
-* Limitations:
-* HTML elements can't have sandbox tag enabled. -- may create an issue with @axe-core/puppeteer team.
-* note: make sure to run node_modules/puppeteer/install.js to get both firefox and chrome switching available
-* Using the await Promise.allSettled(...) allows for testing multiple links at a time.
-* */
+
 
 class AxeRunner {
   run(name, tags, url_list) {
@@ -20,7 +32,6 @@ class AxeRunner {
       headless: true,
     }
     let results = [];
-
     (async () => {
       const browser = await puppeteer.launch({config});
       results = (await Promise.allSettled(
@@ -50,7 +61,10 @@ class AxeRunner {
             }
           }
 
+          // Returns a new promise
           let axe_puppeteer = await new AxePuppeteer(page);
+
+          //if the user selected tags to use, it runs the .withTags() function, otherwise it doesn't.
           if(tags.length > 0){
             axe_puppeteer = axe_puppeteer.withTags(tags);
           }
