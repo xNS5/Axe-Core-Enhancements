b69eb45939acd306b3eaac0faa8a0b2fda646cf3
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-05-31 03:52:17 -0700

    Setting up routing

--- backend/axe-core-enhancements/config/custom.js
+++ backend/axe-core-enhancements/config/custom.js
@@ -18,5 +18,6 @@ module.exports.custom = {
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
+  baseURL: 'http://localhost:1337'
 
 };
