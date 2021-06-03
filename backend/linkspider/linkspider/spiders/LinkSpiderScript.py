661466d746a451222400b12e42d6cd08dc11d095
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-02 19:00:21 -0700

    removed stashed change
    removed stashed change

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -1,20 +1,4 @@
 import scrapy
-<<<<<<< Updated upstream
-
-class HrefSpider(scrapy.Spider):
-    name =  'Links'
-    start_urls = ["https://mywestern.wwu.edu/"]
-
-    def parse(self, response):
-        newLinks = []
-        links = response.xpath('//a/@href')
-        for link in links:
-            url = link.get()
-            if(url[0:5] == 'https'):
-                newLinks.append(url)
-        #print(newLinks)
-        yield newLinks
-=======
 class HrefSpider(scrapy.Spider):
     name =  'Links'
     start_urls = ["https://wandke.com/"]
@@ -28,4 +12,3 @@ class HrefSpider(scrapy.Spider):
                 print("this link past the test", url)
             else:
                 print("this link failed", url)
->>>>>>> Stashed changes
