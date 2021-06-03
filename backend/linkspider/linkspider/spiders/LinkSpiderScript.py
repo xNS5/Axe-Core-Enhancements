08f3f1c1e7db160be7024935f61587453b67c63f
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-02 18:58:31 -0700

    updated the way we sort urls
    updated the way we sort url

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -1,4 +1,5 @@
 import scrapy
+<<<<<<< Updated upstream
 
 class HrefSpider(scrapy.Spider):
     name =  'Links'
@@ -13,3 +14,18 @@ class HrefSpider(scrapy.Spider):
                 newLinks.append(url)
         #print(newLinks)
         yield newLinks
+=======
+class HrefSpider(scrapy.Spider):
+    name =  'Links'
+    start_urls = ["https://wandke.com/"]
+
+    def parse(self, response):
+        print(self.start_urls[0])
+        links = response.xpath('//a/@href')
+        for link in links:
+            url = link.get()
+            if(url[0:len(self.start_urls[0])] == self.start_urls[0]):
+                print("this link past the test", url)
+            else:
+                print("this link failed", url)
+>>>>>>> Stashed changes
