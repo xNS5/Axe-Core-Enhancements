2c304774f7e1b19b58d6e3b5f658161c40d18592
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-08 13:22:07 -0700

    set insead of list

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -3,20 +3,14 @@ class HrefSpider(scrapy.Spider):
     name =  'Links'
     def __init__(self, *args, **kwargs): 
       super(HrefSpider, self).__init__(*args, **kwargs) 
-
       self.start_urls = [kwargs.get('start_urls')] 
     
     def parse(self, response):
-        validLinks = [];
+        validLinks = {self.start_urls[0]}
         links = response.xpath('//a/@href')
-        validLinks.append(self.start_urls[0])
         for link in links:
+            print(link)
             url = link.get()
             if(url[0:len(self.start_urls[0])] == self.start_urls[0]):
-                a = 0
-                for savedurls in validLinks:
-                    if url == savedurls:
-                        a = 1
-                if a == 0:
-                    validLinks.append(url)
+                validLinks.add(str(url))
         print(validLinks)
