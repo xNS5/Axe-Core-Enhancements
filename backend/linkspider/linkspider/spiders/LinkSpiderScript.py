da56954884526998685639eeeb6c7e2e1bc218ec
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-08 16:12:00 -0700

    updaed to handel cases where href are not link and just /pagetitle

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -1,16 +1,30 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+from urllib.parse import urljoin
+
+
+
 class HrefSpider(scrapy.Spider):
     name =  'Links'
+    allowed_domains = []
     def __init__(self, *args, **kwargs): 
       super(HrefSpider, self).__init__(*args, **kwargs) 
-      self.start_urls = [kwargs.get('start_urls')] 
+      self.start_urls = [kwargs.get('start_urls')]
+      self.allowed_domains = [kwargs.get('allowed_domains')]
     
+    rules = (
+        Rule(LinkExtractor(allow=allowed_domains), callback='parse', follow=True),
+    )
+
     def parse(self, response):
         validLinks = {self.start_urls[0]}
-        links = response.xpath('//a/@href')
-        for link in links:
-            print(link)
-            url = link.get()
-            if(url[0:len(self.start_urls[0])] == self.start_urls[0]):
-                validLinks.add(str(url))
+        for allowed_domain in self.allowed_domains:
+            for url in response.xpath('//a/@href').extract():
+                if(url[0] != '#'):
+                    newurl = urljoin(response.url, url)
+                    if allowed_domain in newurl:
+                        if(newurl[0:5] == 'https'):
+                            print(newurl)
+                            validLinks.add(newurl)
         print(validLinks)
