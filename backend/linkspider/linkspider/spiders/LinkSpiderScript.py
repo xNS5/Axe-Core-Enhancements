c1d01650709f01813a232f248a04e8cfe6424388
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-08 11:26:26 -0700

    made it so that link is set in the commandline

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -1,10 +1,12 @@
 import scrapy
 class HrefSpider(scrapy.Spider):
     name =  'Links'
-    start_urls = ["https://wandke.com/"]
+    def __init__(self, *args, **kwargs): 
+      super(HrefSpider, self).__init__(*args, **kwargs) 
 
+      self.start_urls = [kwargs.get('start_urls')] 
+    
     def parse(self, response):
-        #print(self.start_urls[0])
         validLinks = [];
         links = response.xpath('//a/@href')
         validLinks.append(self.start_urls[0])
@@ -15,10 +17,6 @@ class HrefSpider(scrapy.Spider):
                 for savedurls in validLinks:
                     if url == savedurls:
                         a = 1
-                        #print("this link failed", url)
                 if a == 0:
                     validLinks.append(url)
-                    #print("this link past the test", url)
-            #else:
-                #print("this link failed", url)
         print(validLinks)
