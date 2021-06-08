6f3772a0402878df35f4f1280b1d071247220ebb
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-06-08 13:02:09 -0700

    Adding spider controller to repo

--- backend/axe-core-enhancements/api/controllers/spider/SpiderControllerController.js
+++ backend/axe-core-enhancements/api/controllers/spider/SpiderControllerController.js
@@ -4,8 +4,9 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const {PythonShell} = require('python-shell');
 module.exports = {
+
 
   runSpider: function (inputs){
 
