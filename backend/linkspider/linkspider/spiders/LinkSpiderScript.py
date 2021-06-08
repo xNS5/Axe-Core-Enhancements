ad36f479ba4b78f491967ce79afc65d360cedec2
omcoleman2109 <46503891+omcoleman2109@users.noreply.github.com>
2021-06-07 21:27:15 -0700

    made it check if link alredy been added to list

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -4,11 +4,21 @@ class HrefSpider(scrapy.Spider):
     start_urls = ["https://wandke.com/"]
 
     def parse(self, response):
-        print(self.start_urls[0])
+        #print(self.start_urls[0])
+        validLinks = [];
         links = response.xpath('//a/@href')
+        validLinks.append(self.start_urls[0])
         for link in links:
             url = link.get()
             if(url[0:len(self.start_urls[0])] == self.start_urls[0]):
-                print("this link past the test", url)
-            else:
-                print("this link failed", url)
+                a = 0
+                for savedurls in validLinks:
+                    if url == savedurls:
+                        a = 1
+                        #print("this link failed", url)
+                if a == 0:
+                    validLinks.append(url)
+                    #print("this link past the test", url)
+            #else:
+                #print("this link failed", url)
+        print(validLinks)
