1a79da4981b894bd624b441d40dbbc27003ca403
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-06-17 13:41:24 -0700

    Adding spider controller

--- backend/axe-core-enhancements/api/controllers/SpiderController.js
+++ backend/axe-core-enhancements/api/controllers/SpiderController.js
@@ -5,12 +5,19 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 const {PythonShell} = require('python-shell');
+// scrapy crawl Links -a start_urls='https://www.primebellingham.com/'
 module.exports = {
+  friendlyName: 'ace-spider',
+  description: 'calls a python script to crawl through a website for URLS that match its base url',
 
-
-  runSpider: function (inputs){
+  inputs: {
+    url:{
+      type: 'string',
+      required: true
+    }
+  },
+  runSpider: function (inputs, req, res){
 
   }
-
 };
 
