0e665b9d552d531e041326a461d0316ffe9c26f4
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-06-17 02:02:31 -0700

    Converted spider to runnable python script

--- backend/linkspider/linkspider/spiders/LinkSpiderScript.py
+++ backend/linkspider/linkspider/spiders/LinkSpiderScript.py
@@ -1,9 +1,11 @@
+import sys
 import scrapy
+from scrapy import signals
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.crawler import CrawlerProcess
 from urllib.parse import urljoin, urlparse
 from pydispatch import dispatcher
-from scrapy import signals
 from json import JSONEncoder
 
 
@@ -16,28 +18,42 @@ class HrefSpider(CrawlSpider):
     name = 'Links'
     allowed_domains = []
     valid_links = set()
+    url = None
 
-    def __init__(self, *args, **kwargs):
-        super(HrefSpider, self).__init__(*args, **kwargs)
-        self.start_urls = [kwargs.get('start_urls')]
-        domain = urlparse(self.start_urls[0]).netloc
+    def __init__(self,url=None,**kwargs):
+        super(HrefSpider, self).__init__(**kwargs)
+        if url:
+            self.url = url
+            domain = urlparse(self.url).netloc
+            if "www." in domain:
+                self.allowed_domains = [domain[4:]]
+            else:
+                self.allowed_domains = [domain]
+            dispatcher.connect(self.close, signals.spider_closed)
 
-        if "www." in domain:
-            self.allowed_domains = [domain[4:]]
-        else:
-            self.allowed_domains = [domain]
-        dispatcher.connect(self.close, signals.spider_closed)
-    rules = (
+    _rules = (
         Rule(LinkExtractor(allow=allowed_domains), callback='parse', follow=True),
     )
+
+    custom_settings = {
+        'DEPTH_LIMIT': 100,
+    }
+
+    def start_requests(self):
+        return [scrapy.Request(self.url, callback=self.parse)]
 
     def close(self, reason):
         json_str = serialize().encode(self.valid_links)
         print(json_str)
 
     def parse(self, response, **kwargs):
-        for url in response.xpath('//a/@href').extract():
+        for url in response.xpath('//@href').extract():
             newurl = urljoin(response.url, url)
             if self.allowed_domains[0] in newurl and "https" in newurl:
                 self.valid_links.add(newurl)
-                scrapy.http.Request(newurl)
+                yield scrapy.Request(newurl, callback=self.parse)
+
+
+process = CrawlerProcess()
+process.crawl(HrefSpider, url=sys.argv[1])
+process.start()
