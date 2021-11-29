d056816e8ddf0e00d78c731b86a92316d0f15152
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-11-28 18:24:22 -0800

    Adding shell test scripts

---
+++ backend/lib/tests/post-request-sites-desktop-2.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+curl -H "Content-Type: application/json" --request POST --data '{ "engine": "axecore", "browser": "firefox", "a3": "false","wcagLevel": [ "wcag2", "wcag21" ], "criteria": [ "a", "aa" ], "resolution": [ "desktop" ], "urls": [{ "url": "https://primebellingham.com" },{ "url": "https://bellingham.com" }]}' http://localhost:1337/api/v1/axe/axe-runner
+
