ce8352ea97b6db348c7014356cf659b35a08dc9a
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-11-29 02:02:36 -0800

    Adding files for stress testing

---
+++ backend/lib/tests/post-request-sites-desktop-mobile-5-ff.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+curl -H "Content-Type: application/json" --request POST --data '{ "engine": "axecore", "browser": "firefox", "a3": "false","wcagLevel": [ "wcag2", "wcag21" ], "criteria": [ "a", "aa" ], "resolution": [ "desktop","mobile" ], "urls": [{ "url": "https://primebellingham.com" },{ "url": "https://bellingham.com" },{"url":"https://reddit.com"},{"url":"https://mynoise.net"},{"url":"https://www.wwu.edu/"}]}' http://localhost:1337/api/v1/axe/axe-runner
+
