430bc4e3643e7bb722e3eb7a9d89c314e152f032
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-11-29 17:44:06 -0800

    Blah

--- backend/lib/tests/spider/spider-test-1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -H "Content-Type: application/json" --request POST --data  '[{ "url": "https://wandke.com" }, { "depth": "0"}]' http://localhost:1337/api/v1/spider/spider-runner
-
